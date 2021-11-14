a6a0ef9468d3c3b461bfb51ee33742ae38f23528
Isthis <rjsgmldnwn@gmail.com>
2021-11-14 14:54:20 +0900

    Update hanghaeTest_2.js

--- Algorithm/hanghaeTest_2.js
+++ Algorithm/hanghaeTest_2.js
@@ -2,11 +2,11 @@
 //입력 : 100,000,000 이하의 자연수
 //출력 : 각 자릿수를 뒤집어서 더하는 과정과 값이 담긴 수식을 문자열로 출력
 //로직 :
-//1. 자연수를 문자열로 바꾼 후 뒤집는다.
-//2. 각 자릿수를 더하는 수식을 문자열로 만든다.(map을 이용해서 접근)
+//1. 자연수를 문자열로 바꾼 후 나눠준다.
+//2. 반전된 각 자릿수를 더하는 수식을 문자열로 만든다.(map을 이용해서 접근)
 //3. 각 자릿수를 더한 값을 구한다.
 //4. 수식과 결과값을 더한 문자열을 return.
-const n = 718253;
+const n = 100000000;
 
 function solution(n) {
   let request = baseStr(n);
@@ -15,14 +15,14 @@ function solution(n) {
 
   function baseStr(num) {
     num = num + "";
-    num = num.split("")
+    num = num.split("");
     return num;
   }
 
   function reverse(num) {
     let sum = "";
     num = num.reverse().map((str) => (sum = sum + str + "+"));
-    return sum.slice(0,-1);
+    return sum.slice(0, -1);
   }
 
   function sum(num) {
