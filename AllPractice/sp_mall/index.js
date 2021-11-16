16d5aee12c213152188a21e582667f89db68d2c8
Isthis <rjsgmldnwn@gmail.com>
2021-11-16 18:27:33 +0900

    update sp_mall

--- AllPractice/sp_mall/index.js
+++ AllPractice/sp_mall/index.js
@@ -1,15 +1,82 @@
-function getAgeAverage(personArray) {
-  let sum = 0;
-  personArray.forEach((info) => (sum = sum + info["age"]));
-  return sum / personArray.length;
-}
+const express = require("express");
+const app = express();
+const port = 3000;
 
-var personArray = [
-  { name: "John Doe", age: 20 },
-  { name: "Jane Doe", age: 19 },
-  { name: "Fred Doe", age: 32 },
-  { name: "Chris Doe", age: 45 },
-  { name: "Layla Doe", age: 37 },
-];
+const goodsRouter = require("./routes/goods");
+const userRouter = require("./routes/user");
 
-console.log(getAgeAverage(personArray)); // 30.6
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/goods", goodsRouter);
+app.use("/user", userRouter);
+
+app.use((req, res, next) => {
+  console.log(req);
+  next();
+});
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/test", (req, res) => {
+  let name = req.query.name;
+  res.render("test", { name });
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    '<!DOCTYPE html>\
+    <html lang="en">\
+    <head>\
+        <meta charset="UTF-8">\
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">\
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+        <title>Document</title>\
+    </head>\
+    <body>\
+        Hi. I am with html<br>\
+        <a href="/hi">Say Hi!</a>\
+    </body>\
+    </html>'
+  );
+});
+
+app.get("/home", (req, res) => {
+  res.render("index");
+});
+
+app.get("/detail", (req, res) => {
+  res.render("detail");
+});
+// app.get("/goods/list", (req, res) => {
+//   res.send("상품 목록 페이지");
+// });
+
+// app.get("/goods/detail", (req, res) => {
+//   res.send("상품 상세 페이지");
+// });
+
+// app.get("/user/login", (req, res) => {
+//   res.send("로그인 페이지");
+// });
+
+// app.get("/user/register", (req, res) => {
+//   res.send("회원가입 페이지");
+// });
+
+const mongoose = require("mongoose");
+
+app.get("/mongodb", async (req, res) => {
+  await mongoose.connect("mongodb://localhost/voyage", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  res.send("ok");
+});
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
