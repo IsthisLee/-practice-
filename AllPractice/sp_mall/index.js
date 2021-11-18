7335261420be5bdf3b34eaa2e95c9176794152db
Isthis <rjsgmldnwn@gmail.com>
2021-11-18 10:57:57 +0900

    Update index.js

--- AllPractice/sp_mall/index.js
+++ AllPractice/sp_mall/index.js
@@ -19,6 +19,7 @@ app.use("/api", [goodsRouter]);
 // app.use("/goods", goodsRouter);
 // app.use("/user", userRouter);
 
+//Middleware
 app.use((req, res, next) => {
   console.log(req);
   next();
@@ -30,24 +31,6 @@ app.set("view engine", "ejs");
 app.get("/test", (req, res) => {
   let name = req.query.name;
   res.render("test", { name });
-});
-
-app.get("/", (req, res) => {
-  res.send(
-    '<!DOCTYPE html>\
-    <html lang="en">\
-    <head>\
-        <meta charset="UTF-8">\
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">\
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-        <title>Document</title>\
-    </head>\
-    <body>\
-        Hi. I am with html<br>\
-        <a href="/hi">Say Hi!</a>\
-    </body>\
-    </html>'
-  );
 });
 
 app.get("/home", (req, res) => {
