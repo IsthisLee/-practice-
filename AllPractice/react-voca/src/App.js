ff1bfe7f5a3290811b0893f1afe6ac4dc3ba0c0c
IsthisLee <rjsgmldnwn@gmail.com>
2022-05-24 23:59:14 +0900

    learn react basics[react-router-dom-1]

--- AllPractice/react-voca/src/App.js
+++ AllPractice/react-voca/src/App.js
@@ -1,6 +1,7 @@
 import Header from './component/Header';
 import DayList from './component/DayList';
 import Day from './component/Day';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
