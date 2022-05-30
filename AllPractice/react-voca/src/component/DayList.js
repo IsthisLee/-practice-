39897525c5a7bf49889dc8c1166d407ee0ac4c40
IsthisLee <rjsgmldnwn@gmail.com>
2022-05-30 23:59:19 +0900

    (React): learn useEffect(), fetch()

--- AllPractice/react-voca/src/component/DayList.js
+++ AllPractice/react-voca/src/component/DayList.js
@@ -1,14 +1,27 @@
-import dummy from '../db/data.json';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function DayList() {
+  const [days, setDays] = useState([]);
+  const [count, setCount] = useState(0);
+
+  function onClick() {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    console.log('Count change');
+  });
   return (
-    <ul className="list_day">
-      {dummy.days.map(day => (
-        <li key={day.id}>
-          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list_day">
+        {days.map(day => (
+          <li key={day.id}>
+            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={onClick}>{count}</button>
+    </>
   );
 }
