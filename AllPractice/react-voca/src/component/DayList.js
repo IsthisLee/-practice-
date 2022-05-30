20d7a0913b0890957e630443dc58041715fc07e0
IsthisLee <rjsgmldnwn@gmail.com>
2022-05-31 01:10:03 +0900

    (React): learn Custom Hooks

--- AllPractice/react-voca/src/component/DayList.js
+++ AllPractice/react-voca/src/component/DayList.js
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import useFetch from '../hooks/useFetch';
 
 export default function DayList() {
-  const [days, setDays] = useState([]);
+  const days = useFetch('http://localhost:3010/days');
 
-  useEffect(() => {
-    fetch('http://localhost:3010/days')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setDays(data);
-      });
-  }, []);
+  // const [days, setDays] = useState([]);
+  //
+  // useEffect(() => {
+  //   fetch('http://localhost:3010/days')
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       setDays(data);
+  //     });
+  // }, []);
 
   return (
     <ul className="list_day">
