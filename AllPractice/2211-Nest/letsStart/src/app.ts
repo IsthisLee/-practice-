0882a018030d466215f4f5768e4d2dac90caabe9
IsthisLee <rjsgmldnwn@gmail.com>
2022-11-08 02:03:23 +0900

    prac(nest): express + ts 개발 환경 셋업 & hello world!

--- AllPractice/2211-Nest/letsStart/src/app.ts
+++ AllPractice/2211-Nest/letsStart/src/app.ts
@@ -1,1 +1,13 @@
-console.log('hello world!!');
+import * as express from "express";
+
+const app: express.Express = express();
+
+const port: number = 3000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send({ name: "Geonhee Lee", age: 23, friends: ["gh", "ys", "ss"] });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
