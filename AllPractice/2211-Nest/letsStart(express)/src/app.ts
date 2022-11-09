6e0393ae44cb125ce56b4f06ce7af50d1287a504
IsthisLee <rjsgmldnwn@gmail.com>
2022-11-10 00:12:02 +0900

    고양이 데이터 Create Read API 개발

---
+++ AllPractice/2211-Nest/letsStart(express)/src/app.ts
@@ -0,0 +1,98 @@
+import { Cat, CatType } from "./app.model";
+import * as express from "express";
+
+const app: express.Express = express();
+
+const port: number = 8080;
+
+//* 모든 라우터에 적용되는 미들웨어
+app.use((req, res, next) => {
+  console.log("this is logging middleware for all routes");
+
+  next();
+});
+
+//* 해당하는 라우터에만 적용되는 미들웨어
+app.use("/", (req, res, next) => {
+  console.log("this is logging middleware for specific route");
+
+  next();
+});
+
+//* (추가) 이처럼 함수를 통해 모듈화해서 사용할 수도 있겠죠?
+const moduleMiddleware = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  console.log("this is module middleware");
+
+  next();
+};
+
+//* json parsing middleware
+app.use(express.json());
+
+//* READ 고양이 전체 데이터 조회 api
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    res.status(200).send({
+      success: true,
+      data: { cats }
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message
+    });
+  }
+});
+
+//* READ 특정 고양이 데이터 조회 api
+app.get("/cats/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const cat = Cat.find((cat) => cat.id === id);
+
+    res.status(200).send({
+      success: true,
+      data: { cat }
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가 api
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+
+    Cat.push(data); // create a new cat
+
+    res.json({
+      success: true,
+      cats: Cat
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message
+    });
+  }
+});
+
+//* 404 Middleware
+app.get("/*", (req, res, next) => {
+  console.log("this is error middlware");
+
+  res.status(404).send("404 not found error");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
