3bc62db61d0ace083886b78bc1a760da2a5d4f29
IsthisLee <rjsgmldnwn@gmail.com>
2022-12-08 01:04:00 +0900

    고양이 데이터 Update Delete API 개발

--- AllPractice/2211-Nest/letsStart(express)/src/cats/cats.route.ts
+++ AllPractice/2211-Nest/letsStart(express)/src/cats/cats.route.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 
 const router = Router();
 
-//* READ 고양이 전체 데이터 조회 api
+//* READ 고양이 전체 데이터 조회 api -> GET request
 router.get("/cats", (req, res) => {
   try {
     const cats = Cat;
@@ -19,7 +19,7 @@ router.get("/cats", (req, res) => {
   }
 });
 
-//* READ 특정 고양이 데이터 조회 api
+//* READ 특정 고양이 데이터 조회 api -> GET request
 router.get("/cats/:id", (req, res) => {
   try {
     const { id } = req.params;
@@ -37,7 +37,7 @@ router.get("/cats/:id", (req, res) => {
   }
 });
 
-//* CREATE 새로운 고양이 추가 api
+//* CREATE 새로운 고양이 추가 api -> POST request
 router.post("/cats", (req, res) => {
   try {
     const data = req.body;
@@ -47,6 +47,80 @@ router.post("/cats", (req, res) => {
     res.json({
       success: true,
       data: { cats: Cat }
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message
+    });
+  }
+});
+
+//* UPDATE 고양이 데이터 업데이트 -> PUT request
+router.put("/cats/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === id) {
+        cat = body;
+        result = cat;
+      }
+    });
+
+    res.status(201).send({
+      success: true,
+      data: {
+        cat: result
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message
+    });
+  }
+});
+
+//* UPDATE 고양이 데이터 부분 업데이트 -> PATCH request
+router.patch("/cats/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+
+    res.status(201).send({
+      success: true,
+      data: {
+        cat: result
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message
+    });
+  }
+});
+
+//* DELETE 고양이 데이터 삭제 -> DELETE request
+router.delete("/cats/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== id);
+
+    res.status(201).send({
+      success: true,
+      data: {
+        cats: newCat
+      }
     });
   } catch (error) {
     res.status(500).send({
