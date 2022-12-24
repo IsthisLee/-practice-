1e1ea4342ca5c899c0e9cd123070a3d4420e19b5
IsthisLee <rjsgmldnwn@gmail.com>
2022-12-24 17:59:00 +0900

    prac(Nest): Repository 패턴과 레이어 분리

--- AllPractice/2211-Nest/project-1/src/cats/cats.service.ts
+++ AllPractice/2211-Nest/project-1/src/cats/cats.service.ts
@@ -1,17 +1,15 @@
+import { CatsRepository } from './cats.repository';
 import { CatRequestDto } from './dto/cats.request.dto';
 import { ConflictException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Cat } from './cats.schema';
-import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existByEmail(email);
 
     if (isCatExist) {
       throw new ConflictException('해당하는 고양이는 이미 존재합니다.');
@@ -19,7 +17,7 @@ export class CatsService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
+    const cat = await this.catsRepository.create({
       email,
       name,
       password: hashedPassword,
