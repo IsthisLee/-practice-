ef7ebd98b0bb19ada55ccdcdbc7090aec61587ca
IsthisLee <rjsgmldnwn@gmail.com>
2023-06-04 01:44:01 +0900

    prac(TS): 타입스크립트 모듈 - 2 - namepace와 모듈
    Signed-off-by: IsthisLee <rjsgmldnwn@gmail.com>

--- AllPractice/2023/ts-study-2305/job-skills/main.ts
+++ AllPractice/2023/ts-study-2305/job-skills/main.ts
@@ -1,9 +1,13 @@
 // 모듈 불러오는 방법
-// 1.
+// 1. 하나씩 불러오기
 // import { add, minus } from "./module1";
 // console.log(add(1, 2), minus(3, 1));
 
-// 2.
+// 2. 한번에 불러오기
 // 다음과 같이 사용 가능
-import * as MATH from "./module1";
-console.log(MATH.add(1, 2), MATH.minus4(3, 1));
+// import * as MATH from "./module1";
+// console.log(MATH.add(1, 2), MATH.minus4(3, 1));
+
+// 3. namespace 사용하기
+import * as MATH from "./module2";
+console.log(MATH.MyMath.add(1, 2), MATH.MyMath.minus4(3, 1));
---
+++ AllPractice/2023/ts-study-2305/job-skills/module2.ts
@@ -0,0 +1,29 @@
+// namespace 예약어
+// namespace로 묶어서 관리할 수 있다.
+// 장점 - 편리한 관리, 이름 충돌 방지
+// 단점 - 호출하기 번거롭다
+
+// 더 자세히 TS 핸드북을 참고하자.
+export namespace MyMath {
+  export function add(a: number, b: number): number {
+    return a + b;
+  }
+  export function minus(a: number, b: number): number {
+    return a - b;
+  }
+  export function minus2(a: number, b: number): number {
+    return a - b;
+  }
+  export function minus3(a: number, b: number): number {
+    return a - b;
+  }
+  export function minus4(a: number, b: number): number {
+    return a - b;
+  }
+  export function minus5(a: number, b: number): number {
+    return a - b;
+  }
+  export function minus6(a: number, b: number): number {
+    return a - b;
+  }
+}
