9f7d2e7080c7df3a6f3b1c6001a8485128ccff43
IsthisLee <rjsgmldnwn@gmail.com>
2023-06-03 18:54:11 +0900

    prac(TS): ! 과 타입 단언
    Signed-off-by: IsthisLee <rjsgmldnwn@gmail.com>

--- AllPractice/2023/ts-study-2305/job-skills/assert.ts
+++ AllPractice/2023/ts-study-2305/job-skills/assert.ts
@@ -1,2 +1,29 @@
-const testText = "hello world";
-console.log(testText);
+// 1. ! -> 뒤에 !를 붙이면 null이 아니라고 확신하는 것
+/** ex)
+ * const testText = document.querySelector("#someText")!;
+ * console.log(testText?.id);
+ */
+
+// 2. 타입 단언, 타입 캐스팅
+
+// const testText = document.querySelector("#someText");
+// console.log((testText as HTMLElement)?.id);
+
+// const testText2 = document.querySelector("#someText") as HTMLElement;
+// console.log(testText2?.id);
+
+interface IExam {
+  value: number;
+}
+const example: object = { value: 1 };
+
+// 2-1. as
+console.log((example as IExam).value);
+
+const example2 = { value: 1 } as IExam;
+console.log(example2.value);
+
+// 2-2. <> (제네릭)
+console.log((<IExam>example).value);
+
+// --> 제네릭은 나중에 헷갈릴 수 있으니 as를 사용하는 것을 추천
