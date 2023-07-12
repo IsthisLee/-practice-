2716b7ca5656819e29cf64db21e3eeb583f6e426
IsthisLee <rjsgmldnwn@gmail.com>
2023-07-13 00:57:39 +0900

    prac(BE): generic, utility type 추가
    Signed-off-by: IsthisLee <rjsgmldnwn@gmail.com>

--- AllPractice/2023/codecamp/codecamp-backend-online/class2/section08/08-03-typescript-generic/index.ts
+++ AllPractice/2023/codecamp/codecamp-backend-online/class2/section08/08-03-typescript-generic/index.ts
@@ -8,6 +8,7 @@ const result = getPrimitive("hello", 123, true);
 //
 //
 // 2. any 타입(그냥 자바스크립트랑 같음)
+// 함수의 인자로 어떤 타입이 들어갔고 어떤 값이 반환되는지 알 수가 없음
 const getAny = (arg1: any, arg2: any, arg3: any): [any, any, any] => {
   console.log(arg1 + 100); // any는 아무거나 다 됨!
   return [arg3, arg2, arg1];
@@ -18,8 +19,9 @@ const result2 = getAny("hello", "bad", "noAny");
 //
 //
 // 3. unknown 타입
+// 모든 타입이 들어갈 수는 있지만, 아직은 어느 타입을 사용할지 모를때 사용
 const getUnknown = (arg1: unknown, arg2: unknown, arg3: unknown): [unknown, unknown, unknown] => {
-  // any보다는 안전한 코딩
+  // 사용하기 전에 꼭 해당 타입에 대한 정의를 내려주어야 하므로, any보다는 안전한 코딩
   if (typeof arg1 === "number") {
     console.log(arg1 + 100);
   }
@@ -38,7 +40,7 @@ function getGeneric<T1, T2, T3>(arg1: T1, arg2: T2, arg3: T3): [T3, T2, T1] {
 }
 
 const result4 = getGeneric(true, 123, false);
-const result5 = getGeneric<string, number, boolean>("hello", 123, false);
+const result5 = getGeneric<string, number, boolean>("hello", 123, false); // 타입을 지정해주면 해당 타입으로만 사용 가능
 
 //
 //
@@ -48,3 +50,7 @@ const getGeneric2 = <T, U, V>(arg1: T, arg2: U, arg3: V): [V, U, T] => {
 };
 
 const result6 = getGeneric2("", 123, false);
+
+// Generic Type은 우리가 함수를 만들어서 제공해 줄 때 안전하고 확장성 높은 코드 사용을 위해 많이 사용됨
+// 1. 라이브러리 개발시
+// 2. 사내 라이브러리 사용시
--- AllPractice/2023/codecamp/codecamp-backend-online/class2/section08/08-03-typescript-utility/index.ts
+++ AllPractice/2023/codecamp/codecamp-backend-online/class2/section08/08-03-typescript-utility/index.ts
@@ -1,3 +1,6 @@
+// 기존에 있던 Type들을 변형해서 변형된 타입을 새로 만들어주는 역할을 함.
+// 코드의 가독성을 높이기 위해(같은 코드를 여러번 작성하지 않기 위해, 유지보수 편리함을 위해) 많이 사용
+
 interface IProfile {
   name: string;
   age: number;
@@ -6,15 +9,15 @@ interface IProfile {
 }
 
 // 1. Partial 타입
-// 전부 optional로 만들어줌
+// 모든 속성을 optional로 만들어줌
 type aaa = Partial<IProfile>;
 
 // 2. Required 타입
-// 전부 필수로 만들어줌
+// 모든 속성을 필수로 만들어줌
 type bbb = Required<IProfile>;
 
 // 3. Pick 타입
-// 특정 타입만 뽑아서 만들어줌
+// 특정 속성만 뽑아서 만들어줌
 type ccc = Pick<IProfile, "name" | "hobby">;
 
 // 4. Omit 타입
@@ -23,6 +26,7 @@ type ddd = Omit<IProfile, "school">;
 
 // 5. Record 타입
 // 특정 타입을 키로 사용해서 만들어줌
+// Record<Key, Type>으로 사용하며, Key로 들어온 타입을 Type 값을 가지는 타입으로 만들어줌
 
 type eee = "철수" | "영희" | "훈이"; // Union 타입(합집합)
 let child1: eee = "철수"; // -> 철수, 영희, 훈이만 됨. string보다 좁고 안전한 타입
@@ -31,7 +35,7 @@ let child2: string = "사과"; // -> 철수, 영희, 훈이, 사과, 바나나 �
 type fff = Record<eee, IProfile>; // Record 타입
 
 // 6. keyof
-// 객체의 key들로 Union 타입 만들기
+// 객체의 key 값으로 Union 타입 만들기
 type ggg = keyof IProfile; // => "name" | "age" | "school" | "hobby"
 const myProfile: ggg = "hobby";
 
