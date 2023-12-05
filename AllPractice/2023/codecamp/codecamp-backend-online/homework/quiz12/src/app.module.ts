554849a2c862f5bdcb5d731dc7e27ef133b7ae8e
IsthisLee <rjsgmldnwn@gmail.com>
2023-12-06 08:00:56 +0900

    prac(BE): quiz12 - graphql

---
+++ AllPractice/2023/codecamp/codecamp-backend-online/homework/quiz12/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/gtaphql/schema.gql',
+    }),
+  ],
+  providers: [AppResolver, AppService],
+})
+export class AppModule {}
