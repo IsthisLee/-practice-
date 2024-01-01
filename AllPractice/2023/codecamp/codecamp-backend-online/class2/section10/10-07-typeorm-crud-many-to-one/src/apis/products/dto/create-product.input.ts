71ad59ef972464f404f237e32e0d11ae5a665bb3
IsthisLee <rjsgmldnwn@gmail.com>
2024-01-02 08:44:11 +0900

    prac(BE): 1:N 관계

---
+++ AllPractice/2023/codecamp/codecamp-backend-online/class2/section10/10-07-typeorm-crud-many-to-one/src/apis/products/dto/create-product.input.ts
@@ -0,0 +1,22 @@
+import { ProductSalesLocationInput } from '../../productsSalesLocations/dto/product-sales-location.input';
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { Min } from 'class-validator';
+
+@InputType()
+export class CreateProductInput {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => String)
+  description: string;
+
+  @Field(() => Int)
+  @Min(0, { message: '가격은 0원 이상이어야 합니다.' })
+  price: number;
+
+  @Field(() => ProductSalesLocationInput)
+  productSalesLocation: ProductSalesLocationInput;
+
+  @Field(() => String)
+  productCategoryId: string;
+}
---
+++ AllPractice/2023/codecamp/codecamp-backend-online/class2/section10/10-07-typeorm-crud-many-to-one/src/apis/products/entities/product.entity.ts
@@ -0,0 +1,69 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ProductCategory } from 'src/apis/productsCategories/entities/productCategory.entity';
+import { ProductSalesLocation } from 'src/apis/productsSalesLocations/entities/productSalesLocation.entity';
+import { ProductTag } from 'src/apis/productsTags/entities/productTag.entity';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType() // GraphQL 스키마 자동 생성을 위한 데코레이터
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  name: string; // default varchar(255)
+
+  @Column()
+  @Field(() => String)
+  description: string;
+
+  @Column()
+  @Field(() => Int)
+  price: number;
+
+  @Column({ default: false })
+  @Field(() => Boolean)
+  isSoldout: boolean; // default tinyint(1)
+
+  @JoinColumn() // Product 테이블이 중심이다.(외래키가 Product 테이블에 생성됨) -> 1:1 관계에서만 사용
+  @OneToOne(() => ProductSalesLocation) // ProductSalesLocation 테이블과 1:1 관계
+  @Field(() => ProductSalesLocation)
+  productSalesLocation: ProductSalesLocation;
+
+  @ManyToOne(() => ProductCategory)
+  @Field(() => ProductCategory)
+  productCategory: ProductCategory;
+
+  @ManyToOne(() => User)
+  @Field(() => User)
+  user: User;
+
+  @JoinTable() // Product와 ProductTag 사이 중간 테이블이 자동 생성됨.
+  @ManyToMany(() => ProductTag, (productTags) => productTags.products)
+  @Field(() => [ProductTag])
+  productTags: ProductTag[];
+
+  // @CreateDateColumn()
+  // createdAt: Date;
+
+  // @UpdateDateColumn()
+  // updatedAt: Date;
+
+  @DeleteDateColumn() // soft delete
+  deletedAt: Date;
+}
